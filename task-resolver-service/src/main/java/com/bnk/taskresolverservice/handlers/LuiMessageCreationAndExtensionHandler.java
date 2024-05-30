2b42297e9047b68d0af1dc8fa55e9fd082b879c8
sepxx <mihartthx@gmail.com>
2024-05-30 17:53:24 +0300

    привел код handler в порядок

--- task-resolver-service/src/main/java/com/bnk/taskresolverservice/handlers/LuiMessageCreationAndExtensionHandler.java
+++ task-resolver-service/src/main/java/com/bnk/taskresolverservice/handlers/LuiMessageCreationAndExtensionHandler.java
@@ -5,64 +5,41 @@ import com.bnk.taskresolverservice.clients.RecipientsSaverServiceRestClient.Reci
 import com.bnk.taskresolverservice.dtos.ListInfoUpdateEventType;
 import com.bnk.taskresolverservice.dtos.ListInfoUpdateMessage;
 import com.bnk.taskresolverservice.dtos.RecipientDto;
-import com.bnk.taskresolverservice.entities.Recipient;
 import com.bnk.taskresolverservice.entities.RecipientList;
 import com.bnk.taskresolverservice.mappers.RecipientRecipientDtoMapper;
 import com.bnk.taskresolverservice.repositories.RecipientListRepository;
-import jakarta.persistence.EntityManager;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
 
-import java.util.Optional;
-
-import static org.springframework.transaction.annotation.Propagation.NOT_SUPPORTED;
-
-
-//TODO: а мб тут рубильник какой-то сделать с проверкой существования списка?
-//мб ситуация пользователь создал список и тут же его удалил
-//а TRS будет пытаться затянуть его себе
-//КОРОЧЕ ГОВОРЯ ОБДУМАТЬ
-
-/* создание:
-   создание списка
-   опрос RS и дополнение
-*
-*/
-
 @Slf4j
 @Component
 @AllArgsConstructor
-@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
-public class LuiMessageCreateHandler implements LuiMessageHandler {
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LuiMessageCreationAndExtensionHandler implements LuiMessageHandler {
 
-    RecipientListRepository recipientListRepository;
-    RecipientSaverServiceRestClient recipientSaverServiceRestClient;
-    RecipientRecipientDtoMapper recipientRecipientDtoMapper;
-
-    static Integer REQUEST_PAGE_SIZE = 10; //TODO: вынос в properties
+    final RecipientListRepository recipientListRepository;
+    final RecipientSaverServiceRestClient recipientSaverServiceRestClient;
+    final RecipientRecipientDtoMapper recipientRecipientDtoMapper;
+    @Value("${recipients-service.request_page_size}")
+    static Integer REQUEST_PAGE_SIZE;
     @Override
     public Boolean canHandle(ListInfoUpdateMessage message) {
-        return ListInfoUpdateEventType.CREATION.equals(message.getEventType());
+        return ListInfoUpdateEventType.CREATION.equals(message.getEventType()) || ListInfoUpdateEventType.EXTENSION.equals(message.getEventType());
     }
 
     @Override
     public void handle(ListInfoUpdateMessage message) {
         String listName = message.getListName();
         String userId = message.getUserId();
-        System.out.println("CREATION CREATION CREATION CREATION CREATION");
+        log.info("!!! IN {} HANDLER !!!", message.getEventType());
         RecipientList recipientList = recipientListRepository.findByNameAndUserId(listName, userId)
                 .orElseGet(()->new RecipientList(listName, userId));
 
