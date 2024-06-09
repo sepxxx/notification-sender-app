cec0bb0159bfd1fb3b1b319e01e640b008f15bde
xxx <xxx@>
2024-06-09 11:39:08 +0300

    похоже заработал keycloak

--- nsa-client/src/main.js
+++ nsa-client/src/main.js
@@ -3,11 +3,33 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI);
+
+import VueKeycloakJs from '@dsb-norge/vue-keycloak-js'
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+Vue.use(VueKeycloakJs, {
+  init: {
+    // Use 'login-required' to always require authentication
+    // If using 'login-required', there is no need for the router guards in router.js
+    // onLoad: 'check-sso',
+    onLoad: 'login-required',
+    // silentCheckSsoRedirectUri: window.location.origin + "/silent-check-sso.html"
+  },
+  config: {
+    url: 'http://localhost:8180/',
+    clientId: 'spring-with-test-scope',
+    realm: 'demo'
+  },
+  onReady: kc => {
+    new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+    console.log(kc);
+  }
+})
 
-
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount('#app')
--- nsa-client/src/router.js
+++ nsa-client/src/router.js
@@ -1,37 +1,108 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MainPage from './views/MainPage.vue'
-import MailingsPage from './views/MailingsPage.vue'
-import MailingsCreationPage from './views/MailingsCreationPage.vue'
-import ListsPage from './views/ListsPage.vue'
-import OffersPage from "@/views/OffersPage.vue";
+// import MailingsPage from './views/MailingsPage.vue'
+// import MailingsCreationPage from './views/MailingsCreationPage.vue'
+// import ListsPage from './views/ListsPage.vue'
+// import OffersPage from "@/views/OffersPage.vue";
+
+// export default new Router({
+//     mode: 'history',
+//     routes: [
+//         {
+//             path: '/',
+//             component: MainPage
+//         },
+//         {
+//             path: "/mailings",
+//             component: MailingsPage
+//
+//         },
+//         {
+//             path: "/lists",
+//             component: ListsPage
+//         },
+//         {
+//             path: "/mailings/creation",
+//             component: MailingsCreationPage
+//
+//         },
+//         {
+//             path: "/templates/offers",
+//             component: OffersPage
+//
+//         }
+//     ]
+// })
+//
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     mode: 'history',
+    base: '/',
     routes: [
         {
             path: '/',
+            name: 'MainPage',
             component: MainPage
         },
         {
-            path: "/mailings",
-            component: MailingsPage
-
-        },
+            path: '/mailings',
+            name: 'MailingsPage',
+            component: () => import(/* webpackChunkName: "secret" */ './views/MailingsPage.vue'),
+            // meta: {
+            //     requiresAuth: true
+            // }
+        }
+        ,
         {
-            path: "/lists",
-            component: ListsPage
-        },
+            path: '/lists',
+            name: 'ListsPage',
+            component: () => import(/* webpackChunkName: "secret" */ './views/ListsPage.vue'),
+            // meta: {
+            //     requiresAuth: true
+            // }
+        }
+        ,
         {
-            path: "/mailings/creation",
-            component: MailingsCreationPage
-
-        },
+            path: '/mailings/creation',
+            name: 'MailingsCreationPage',
+            component: () => import(/* webpackChunkName: "secret" */ './views/MailingsCreationPage.vue'),
+            // meta: {
+            //     requiresAuth: true
+            // }
+        }
+        ,
         {
-            path: "/templates/offers",
-            component: OffersPage
-
+            path: '/templates/offers',
+            name: 'OffersPage',
+            component: () => import(/* webpackChunkName: "secret" */ './views/OffersPage.vue'),
+            // meta: {
+            //     requiresAuth: true
+            // }
         }
     ]
 })
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+router.beforeEach(async (to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        // We wait for Keycloak init, then we can call all methods safely
+        while (router.app.$keycloak.createLoginUrl === null) {
+            await sleep(100)
+        }
+        if (router.app.$keycloak.authenticated) {
+            next()
+        } else {
+            const loginUrl = router.app.$keycloak.createLoginUrl()
+            window.location.replace(loginUrl)
+        }
+    } else {
+        next()
+    }
+})
+
+export default router
